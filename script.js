7623aa4ed57ed6f6bd02d22725e0f6dc103529c3
Maurizio Palmisano <maurizio.palmisano1@gmail.com>
2026-03-23 16:49:42 +0100

    creazioni delle variabili e del primo controllo sulle variabili

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 'use strict'
 
+const tariffa_base = 0.21;
+const km_viaggio_stringa = prompt('iserire la distanza del viaggio in Km');
+const etá_passeggero_stringa = prompt("iserire l'etá del passeggero");
+const km_viaggio = parseInt(km_viaggio_stringa);
+const etá_passeggero = parseInt(etá_passeggero_stringa);
+
+if (!(isNaN(km_viaggio)) && !(isNaN(km_viaggio))) {
+    
+} else{
+    console.error("L'etá inserita o la distanza del viaggio non sono corrette.");
+    
+}
+
